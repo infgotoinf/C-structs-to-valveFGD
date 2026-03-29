731a02f73038551b71d2cb3a73a21da58fd0d989
-inf <infgotoinf@gmail.com>
2026-03-29 22:23:47 +0300

    Write the regexp, make basic functionality so you can export file in a specified directory or dest

--- main.pl
+++ main.pl
@@ -2,8 +2,10 @@
 
 use v5.42;
 use utf8;
-binmode STDOUT, ":encode(UTF-8)";
-binmode STDIN, ":encode(UTF-8)";
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDIN, ':encoding(UTF-8)';
+use File::Spec;
+# use Term::ReadKey;
 
 
 sub printHelp {
@@ -14,6 +16,71 @@ sub printHelp {
 
 
 die &printHelp if @ARGV != 2;
+my $source = $ARGV[0];
+my $dest = $ARGV[1];
 
-open (my $source, '<', $ARGV[0])
-    or die "Could not open source file: $!";
+if (-d $dest) {
+  $dest = File::Spec->catfile( $dest, $source =~ s/\..*\Z/.fgd/r );
+}
+
+# TODO: Figure out with y/n prompt
+if (-e $dest) {
+    print "Sure you want to overwrite $dest [y/N]? ";
+
+    ...
+
+    # ReadMode 4;
+    # my $key = '';
+    # while (($key = ReadKey(-1)) ne 'y') {
+    # }
+    # ReadMode 0;
+}
+# open (my $dest_file, '>'
+
+open (my $source_file, '<', $source)
+    or die "Could not open source file: $!\n";
+
+# open (my $dest_file, '>', $dest)
+#     or die "Could not create dest file: $!\n";
+
+say "Source: $source";
+say "Dest: $dest";
+
+
+# Put all lines from file into variable
+my $source_code = join '', <$source_file>;
+say $source_code;
+
+# Remove all macro definitions and includes if any
+$source_code =~ s/^\s*#.*$//gm;
+
+
+
+$source_code =~ s{
+(?:
+    (?:
+        \s* //\s* (?<struct_comment>[^\n]*) \n
+    )?
+
+    (?<struct>
+        \s* struct\s+ (?<struct_name>\w+\d?)\s* \{
+            .*?
+            \s* (?<var_type>\w+)\s+ (?<var_name>\w+);
+            .*?
+        \};
+    )
+)
+
+}{\@PointClass size(-16 -16 -32, 16 16 32) = $+{struct_name} : "$+{struct_comment}"
+[
+    $+{var_name}($+{var_type})
+]
+
+}mgxs;
+
+say $source_code;
+
+
+open (my $dest_file, '>', $dest)
+    or die "Could not create dest file: $!\n";
+print $dest_file, $source_code;
